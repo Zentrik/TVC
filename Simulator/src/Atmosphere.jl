3babe6e7b06e8a61b4f7172a118d4846fdd5a1be
Zentrik <Zentrik@users.noreply.github.com>
2022-04-08 13:35:40 +0100

    Remove Simulator Package

--- Simulator/src/Atmosphere.jl
+++
@@ -1,9 +0,0 @@
-using Parameters, Geophysics
-
-@with_kw struct atmosphere @deftype Function
-    gravity = height -> [0; 0; -gravity(height)]
-    
-    density = Geophysics.density
-    speedOfSound = sonicspeed
-    kinematicViscocity = kinematic
-end
--- Simulator/src/Simulator.jl
+++
@@ -1,5 +0,0 @@
-module Simulator
-
-greet() = print("Hello World!")
-
-end # module
