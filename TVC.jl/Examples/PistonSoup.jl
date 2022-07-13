0550c7fa5f43a8281a141185c54533fc8a0580ec
Zentrik <Zentrik@users.noreply.github.com>
2022-07-13 11:42:11 +0100

    Simulate for correct time and clean up code

--- TVC.jl/Examples/PistonSoup.jl
+++ TVC.jl/Examples/PistonSoup.jl
@@ -7,13 +7,13 @@ using Parameters
     atmos::Atmosphere = Atmosphere(g = height -> [0; 0; 0])
     traj::RocketTrajectoryParameters = RocketTrajectoryParameters()
 
-    Aero::Bool = true
+    Aero::Bool = false
     wind::V = zeros(3)
 
     ground::Bool = false
 
     MotorIgnitionTime::R = 0.0
-    Control = (x, p, t) -> (force=[1; 3; 5], torque=[0.25; 0.5; 0.75])
+    Control = (force=[1; 3; 5], torque=[0.25; 0.5; 0.75])
 end
 
 #   CASE 1
@@ -23,11 +23,12 @@ veh = RocketParameters(BurnTime=10., InertiaTensor=I(3), Mass=t -> 10)
 
 x₀ = [[0; 0; 0]; [0; 0; 0]; [1; 0; 0; 0]; [0; 0; 0]][[veh.id_r; veh.id_v; veh.id_quat; veh.id_ω]];
 
+p = ODEParameters(veh=veh)
+
 #   Solve ODE
 #   ≡≡≡≡≡≡≡≡≡≡≡
 
 tspan = (0, veh.BurnTime)
-p = ODEParameters(veh=veh, Aero=false)
 
 prob = ODEProblem(f!, x₀, tspan, p)
 
@@ -45,22 +46,32 @@ println(sol.u[end][veh.id_ω])
 
 veh = RocketParameters(BurnTime=15., InertiaTensor=[0.7 0 0.7; 0 1 0; 0.7 0 -0.7], Mass=t -> 0.26)
 
-p = ODEParameters(veh=veh, Aero=false, Control=(x, p, t) -> (force=zeros(3), torque=[0; 0; 0.25]))
+x₀ = [[0; 0; 0]; [0; 0; 0]; [1; 0; 0; 0]; [0; 0; 0]][[veh.id_r; veh.id_v; veh.id_quat; veh.id_ω]];
+
+p = ODEParameters(veh=veh, Control=(force=zeros(3), torque=[0; 0; 0.25]))
+
+#   Solve ODE
+#   ≡≡≡≡≡≡≡≡≡≡≡
+
+tspan = (0, veh.BurnTime)
 
 prob = ODEProblem(f!, x₀, tspan, p)
+
+condition(x,t,integrator) = x[3] # when height is zero halt integration
+cb = ContinuousCallback(condition, nothing, terminate!) # when going upwards do nothing
 
 sol = DifferentialEquations.solve(prob, reltol=1e-8, abstol=1e-8, callback=cb)
 
 println(sol.u[end][veh.id_r])
 println(TVC.Utils.to_matrix(sol.u[end][veh.id_quat]))
 println(sol.u[end][veh.id_ω])
-
 #   CASE 3
 #   ≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡≡
 
+veh = RocketParameters(BurnTime=15., InertiaTensor=[0.7 0 0.7; 0 1 0; 0.7 0 -0.7], Mass=t -> 0.26)
+
 x₀ = [[0; 0; 0]; [0; 0; 0]; [1; 0; 0; 0]; [0; 10; 0]][[veh.id_r; veh.id_v; veh.id_quat; veh.id_ω]];
 
-veh = RocketParameters(BurnTime=15., InertiaTensor=[0.7 0 0.7; 0 1 0; 0.7 0 -0.7], Mass=t -> 0.26)
 
 p = ODEParameters(veh=veh, Aero=false, 
     Control=(x, p, t) -> begin
@@ -70,6 +81,11 @@ p = ODEParameters(veh=veh, Aero=false,
             return (force=zeros(3), torque=zeros(3))
         end
     end)
+
+#   Solve ODE
+#   ≡≡≡≡≡≡≡≡≡≡≡
+
+tspan = (0, veh.BurnTime)
 
 prob = ODEProblem(f!, x₀, tspan, p)
 
