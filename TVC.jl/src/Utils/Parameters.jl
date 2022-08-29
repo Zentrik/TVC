1345037ca306ffc854db5dfa4c60a929b05cd80c
Zentrik <Zentrik@users.noreply.github.com>
2022-08-29 17:02:20 +0100

    Don't force Int64

--- TVC.jl/src/Utils/Parameters.jl
+++ TVC.jl/src/Utils/Parameters.jl
@@ -3,7 +3,7 @@ using ..Utils
 
 export RocketParameters
 
-@with_kw struct RocketParameters{R<:Real, M<:AbstractMatrix} @deftype R
+@with_kw struct RocketParameters{R<:Real, M<:AbstractMatrix, I<:Int} @deftype R
     Reference_Diameter = 7.62e-2
     Reference_Radius = Reference_Diameter / 2
     Reference_Area = Reference_Radius^2 * pi
@@ -34,19 +34,19 @@ export RocketParameters
     COTToCG::Any = t -> Total_Length - CG(t)
     MomentArm::Any = t -> [0; 0; -COTToCG(t)]
 
-    id_r::UnitRange{Int64} = 1:3
-    id_v::UnitRange{Int64} = 4:6
-    id_quat::UnitRange{Int64} = 7:10
-    id_ω::UnitRange{Int64} = 11:13
-    id_T::UnitRange{Int64} = 14:16
-    id_Ṫ::UnitRange{Int64} = 17:19
+    id_r::UnitRange{I} = 1:3
+    id_v::UnitRange{I} = 4:6
+    id_quat::UnitRange{I} = 7:10
+    id_ω::UnitRange{I} = 11:13
+    id_T::UnitRange{I} = 14:16
+    id_Ṫ::UnitRange{I} = 17:19
 
-    id_T̈::UnitRange{Int64} = 1:3
-    id_roll::UnitRange{Int64} = 4:4
+    id_T̈::UnitRange{I} = 1:3
+    id_roll::UnitRange{I} = 4:4
 
-    id_tcoast::Int64 = 1
+    id_tcoast::I = 1
 
-    # nx::Int64 = 19
-    # nu::Int64 = 4
-    # np::Int64 = 1
+    # nx::I = 19
+    # nu::I = 4
+    # np::I = 1
 end
